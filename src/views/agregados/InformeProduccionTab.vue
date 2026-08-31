41aea581cbf4ad6f3d0c2255e8fc0c331a26a192
Sys-Tic-GraviconSA <sys.tic@gravicon.com.co>
2026-08-31 16:59:57 -0500

    style(informe): boton descargar produccion Cuncia con estilo tb-btn primary como demas informes

--- src/views/agregados/InformeProduccionTab.vue
+++ src/views/agregados/InformeProduccionTab.vue
@@ -9,7 +9,7 @@
         <select v-model="selectedMonthKey" class="month-select">
           <option v-for="m in availableMonths" :key="m.key" :value="m.key">{{ m.label }}</option>
         </select>
-        <button class="action-btn" @click="generarPdf" :disabled="!hasData || generandoPdf">
+        <button class="tb-btn primary" @click="generarPdf" :disabled="!hasData || generandoPdf">
           <svg v-if="!generandoPdf" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
           <span v-if="generandoPdf">Generando PDF...</span>
           <span v-else>Descargar PDF</span>
